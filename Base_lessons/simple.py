43e5f1ec109e83064d48d2dd535a7f8ce3651060
Vladislav Lukashin <agestop1@rambler.ru>
2022-11-22 16:39:19 +0300

    Добавил все файлы по базовым урокам

---
+++ Base_lessons/simple.py
@@ -0,0 +1,17 @@
+a = 2
+b = 0.5
+name = 'Владислав'
+
+print(a + b)
+print(f'Привет, {name}!')
+
+v = input('Введите число от 1 до 10: ')
+print(int(v) + 10) 
+
+print(f'Привет, {name}, как дела?')
+
+print(float('1'))  # 1
+#print(int('2.5'))  # Ошибка
+print(bool(1))  # True
+print(bool(''))  # False
+print(bool(0))  # False
