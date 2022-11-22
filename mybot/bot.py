963ffb4abe917072b09d69843ae6273beca6e55b
Vladislav Lukashin <agestop1@rambler.ru>
2022-11-22 22:59:45 +0300

    Добавил TG бота

---
+++ mybot/bot.py
@@ -0,0 +1,18 @@
+import logging
+from telegram.ext import Updater, CommandHandler #Импортировали Updater из модуля ext библиотеки Telegram.
+                                 #Этот модуль отвечает за коммуникацию с сервером Telegram - именно он получает/передает сообщения.
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+
+def main():
+    # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
+    mybot = Updater("5941807026:AAHmB8Afi3zf7inQd00zUwtLD003MEL7Ozg", use_context=True)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    # Командуем боту начать ходить в Telegram за сообщениями
+    mybot.start_polling()
+    # Запускаем бота, он будет работать, пока мы его не остановим принудительно
+    mybot.idle()
+
+main()
