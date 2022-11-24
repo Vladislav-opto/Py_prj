d4fdb084d9a8ca4018df5397c3d27b395d9762e2
Vladislav Lukashin <agestop1@rambler.ru>
2022-11-24 23:01:47 +0300

    bot.py

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,18 +1,27 @@
 import logging
-from telegram.ext import Updater, CommandHandler #Импортировали Updater из модуля ext библиотеки Telegram.
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters #Импортировали Updater из модуля ext библиотеки Telegram.
                                  #Этот модуль отвечает за коммуникацию с сервером Telegram - именно он получает/передает сообщения.
+import settings
+logging.basicConfig(filename="bot.log", level=logging.INFO, encoding='utf-8')
+
 def greet_user(update, context):
     print('Вызван /start')
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
 
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
 def main():
     # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
-    mybot = Updater("5941807026:AAHmB8Afi3zf7inQd00zUwtLD003MEL7Ozg", use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     # Командуем боту начать ходить в Telegram за сообщениями
+    logging.info("Бот стартовал!")
     mybot.start_polling()
     # Запускаем бота, он будет работать, пока мы его не остановим принудительно
     mybot.idle()
-
+if __name__ = "__main__":
 main()
