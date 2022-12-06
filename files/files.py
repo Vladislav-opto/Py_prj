917fb5454e3ea0da7de585d03bfd21ed980e4a7d
Vladislav Lukashin <agestop1@rambler.ru>
2022-12-06 15:20:58 +0300

    Исправил files

--- files/files.py
+++ files/files.py
@@ -1,15 +1,21 @@
-def len_qnt_file(my_file):
-    with open(my_file, 'r', encoding='utf-8') as my_file:
-        data_from_file = my_file.read()
-    print(f'Длина старого файла: {len(data_from_file)}')
-    print(f'Количество слов в старом файле: {len(data_from_file.split())}')
+def print_result(preamble, data_to_print):
+    print(f'{preamble}: {data_to_print} ед.')
+
+
+def len_qnt_file(input_file):
+    with open(input_file, 'r', encoding='utf-8') as file_handler:
+        data_from_file = file_handler.read()
+    len_file = len(data_from_file)
+    quantity_word = len(data_from_file.split())
+    print_result('Длина старого файла', len_file)
+    print_result('Количество слов в старом файле', quantity_word)
     data_from_file = data_from_file.replace('.', '!')
-    return(data_from_file)
+    return data_from_file
 
 
-def write_to_file(my_file, data_from_file):
-    with open(my_file, 'w', encoding='utf-8') as my_file:
-        my_file.write(data_from_file)
+def write_to_file(file_output, data_from_file):
+    with open(file_output, 'w', encoding='utf-8') as file_handler:
+       file_handler.write(data_from_file)
 
 
 if __name__ == "__main__":
