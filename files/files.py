6832a4e1b90472607582cec14e95c379e9b7e7f0
Vladislav Lukashin <agestop1@rambler.ru>
2022-12-08 11:18:03 +0300

    Исправил чутка в files

--- files/files.py
+++ files/files.py
@@ -1,16 +1,17 @@
-def print_result(preamble, data_to_print):
-    print(f'{preamble}: {data_to_print} ед.')
+def replace_in_file(input_file):
+    with open(input_file, 'r', encoding='utf-8') as file_handler:
+        data_from_file = file_handler.read()
+    data_from_file = data_from_file.replace('.', '!')
+    return data_from_file
 
 
-def len_qnt_file(input_file):
+def len_qnt_words_file(input_file):
     with open(input_file, 'r', encoding='utf-8') as file_handler:
         data_from_file = file_handler.read()
     len_file = len(data_from_file)
     quantity_word = len(data_from_file.split())
-    print_result('Длина старого файла', len_file)
-    print_result('Количество слов в старом файле', quantity_word)
-    data_from_file = data_from_file.replace('.', '!')
-    return data_from_file
+    len_qnt_words = 'Длина старого файла ' + str(len_file) + '\n' + 'Количество слов в старом файле ' + str(quantity_word)
+    return len_qnt_words
 
 
 def write_to_file(file_output, data_from_file):
@@ -19,4 +20,5 @@ def write_to_file(file_output, data_from_file):
 
 
 if __name__ == "__main__":
-    write_to_file('referat2.txt', len_qnt_file('referat.txt'))
+    print(len_qnt_words_file('referat.txt'))
+    write_to_file('referat2.txt', replace_in_file('referat.txt'))
